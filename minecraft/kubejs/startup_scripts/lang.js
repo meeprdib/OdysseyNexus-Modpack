542ce9013ece387df20f494e9b94916a2283f003
Davey <meeprdib@outlook.com>
2024-12-10 23:12:49 -0500

    Start of salt unification (for real this time)

---
+++ minecraft/kubejs/startup_scripts/lang.js
@@ -0,0 +1,5 @@
+ClientEvents.lang("en_us", event => {
+    event.renameItem('spelunkery:salt', 'Salt Dust')
+    event.renameItem('galosphere:pink_salt_shard', 'Salt Shard')
+    event.renameItem('galosphere:pink_salt_straw', 'Salt Straw')
+})
